a1722eca8d751b7fde8016d9d5adf68c9a4311e0
mohammad.tausif <mohammad.tausif@ozdilek.com.tr>
2023-08-06 21:01:05 +0300

    basic list ui implemented

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var coinList = ['CoinA','CoinB','CoinC','CoinD','CoinE','CoinF','CoinG','CoinB','CoinC','CoinD','CoinF','CoinG','CoinF','CoinR','CoinA','CoinB','CoinC','CoinD','CoinE','CoinF','CoinG','CoinB','CoinC','CoinD','CoinF','CoinG','CoinF','CoinR'];
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +40,29 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Text('Coin Analysis Body'),
+      /*body: ListView.builder(itemBuilder: (context, index){
+        return Text(coinList[index], style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),);
+      },
+      itemCount: coinList.length,
+      ),*/
+      body: ListView.separated(itemBuilder: (context, index){
+          return ListTile(
+            //leading: Text('${index+1}', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+            leading: CircleAvatar(
+              //backgroundColor: Colors.lightGreen,
+              backgroundImage: AssetImage('assets/app_logo.png'),
+              maxRadius: 28,
+            ),
+            title: Text(coinList[index], style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),
+            subtitle: Text('subtitle'),
+            trailing: Icon(Icons.menu),
+          );
+        },
+        itemCount: coinList.length,
+        separatorBuilder: (context, index){
+          return Divider(height: 30, thickness: 2,);
+        },
+      ),
     );
   }
 }
